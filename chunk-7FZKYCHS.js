3ff31e4a95a317b03897e191205e6a1d479f3c7e
Edwin Nuñez <dweniezenun@gmail.com>
2025-07-16 12:59:07 +0200

    Auto-generated commit

--- chunk-7FZKYCHS.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-MCPCYQF5.js";import"./chunk-UAZ2I5FH.js";import"./chunk-FRPYIK7A.js";export{a as SkillsComponent};
--- chunk-GOF6W5MX.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-7GK5KHTP.js";import"./chunk-UAZ2I5FH.js";import"./chunk-FRPYIK7A.js";export{a as ServiceComponent};
